63dbfa0d8856a1cc1ea8d5e303c2ac4a5992ace3
Quentin Ikeno <ikenoq@gmail.com>
2022-06-21 16:37:56 -1000

    Add click handler to change color and msg.

--- src/EightBall.js
+++ src/EightBall.js
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
+import pickRandom from "./helpers";
 import "./EightBall.css";
 
 const EightBall = ({ answers }) => {
 	const [msg, setMsg] = useState("Think of a Question");
 	const [color, setColor] = useState("black");
+	const setMsgAndColor = () => {
+		const { msg, color } = pickRandom(answers);
+		setMsg(msg);
+		setColor(color);
+	};
 	return (
-		<div className={`EightBall-circle ${color}`}>
+		<div className={`EightBall-circle ${color}`} onClick={setMsgAndColor}>
 			<span>{msg}</span>
 		</div>
 	);
